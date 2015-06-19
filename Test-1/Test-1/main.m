1213104d4900e67ce6a8ee5f6d4ee395d208ddee
Crowphy <672570250@qq.com>
2015-06-19 17:37:08 +0800

    12

---
+++ Test-1/Test-1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Test-1
+//
+//  Created by 吴亚辉 on 6/19/15.
+//  Copyright (c) 2015 吴亚辉. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
